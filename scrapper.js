d9e44850761a3c6e824ec459278aa820532d3cb7
unknown <edirkse@vt.edu>
2022-03-21 14:02:04 -0400

    Attempting to load webite into some time of parsable object using a library. Struggling with the libraries and js

--- scrapper.js
+++ scrapper.js
@@ -1,1 +1,34 @@
+//install packages
+//npm install request
+//npm install axios
+//npm install cheerio
 
+//require is a module loader. 
+const axios = require('axios')
+
+axios
+	.get('https://www.reddit.com/r/programming.json')
+	.then((response) => {
+		console.log(response)
+	})
+	.catch((error) => {
+		console.error(error)
+	})
+
+class Scrapper {
+    //class body
+    //define class fields (can also just make them in constructor)
+    name
+
+    //contructor. I believe this is called whenever the class is instantiated
+    // instantiate class via const myScrapper = new Scrapper(); 
+    //js classes can have only one constructor
+    constructor(name) {
+        //create a new field that is the name of the of the scrapper object. 
+        //this is an instance field (on the class instance) (as opposed to class itself (static))
+        this.name = name; 
+
+        
+
+    }
+}
