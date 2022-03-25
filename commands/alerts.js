ce28b9ba49b813508351143fb1358df3137c35a8
ryanmaxey6 <ryanmaxey6@vt.edu>
2022-03-24 21:07:33 -0400

    Moved subscribedUsers object to index.js

--- commands/alerts.js
+++ commands/alerts.js
@@ -1,38 +1,30 @@
 module.exports = {
     name: 'alerts',
     description: "Allows the user to subsribe to alerts.",
-    execute(message, args) {
-        const categories = ['travel', 'vaccines', 'masks'];
-        
+    execute(message, args, subscriptions) {
         // Do we need to store each user's subscriptions?
         // Should the Bot send the alerts to a specific Discord channel?
         // It seems like we wouldn't want every user's alerts to be sent to one
         // channel because users would have to search to find the alerts
         // they're interested in (defeats the purpose of personalized alerts).
         // It may be better to send alerts as direct messages to the users.
-
-        // Bot would probably need to keep a list of subscribed users for each category.
-        const subscribedUsers = {
-            'travel': [],
-            'vaccines': [],
-            'masks': []
-        };
-
+        console.log(message.author);
+        
         if (args.length === 0) {
             message.channel.send('Please include a category to subscribe to: travel, vaccines, masks');
             return;
         }
 
         for (arg of args) {
-            if (!categories.includes(arg)) {
+            if (!Object.keys(subscriptions).includes(arg)) {
                 message.channel.send(`"${arg}" is not a category.`);
+            } else if (subscriptions[arg].has(message.author)) {
+                // need to check if the user is already subscribed
+                message.channel.send(`You are already subscribed to ${arg} alerts.`);
             } else {
                 message.channel.send(`You have subscribed to ${arg} alerts.`);
-                console.log(message.author);
                 // message.author.send(`You have subscribed to ${arg} alerts. To unsubscribe ...`);
-                // need to check if the user is already subscribed
-                subscribedUsers[arg].push(message.author);
-                console.log(subscribedUsers);
+                subscriptions[arg].add(message.author);
             }
         }
     }
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js');
+const { setServers } = require('dns');
 const fs = require('fs');
 
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
-
 const prefix = '$'
-
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -12,6 +11,14 @@ for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
+
+// Bot would probably need to keep a list of subscribed users for each category.
+// Using sets to ensure we don't have duplicates.
+const subscribedUsers = {
+    'travel': new Set(),
+    'vaccines': new Set(),
+    'masks': new Set()
+};
 
 client.once('ready', () => {
     console.log('CovidBot is ready!')
@@ -24,13 +31,13 @@ client.on('messageCreate', message => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    console.log(command);
-    console.log(args);
+    console.log(`Command: ${command}`);
+    console.log(`Arguments: ${args}`);
 
     if(command === 'ping') {
         client.commands.get('ping').execute(message, args);
     } else if (command === 'alerts') {
-        client.commands.get('alerts').execute(message, args);
+        client.commands.get('alerts').execute(message, args, subscribedUsers);
     }
 });
 
