a6dcde32c8a10116949b177c56bd00e611af48e4
Eles Jones <eles12@vt.edu>
2022-04-26 14:36:07 -0400

    added Crohn job

--- index.js
+++ index.js
@@ -43,5 +43,17 @@ client.on('messageCreate', message => {
     }
 });
 
+let scheduledMessage = new cron.CronJob('00 00 08 * * *', () => {
+    // This runs every day at 10:30:00, you can do anything you want
+    // Specifing your guild (server) and your channel
+       const guild = client.guilds.cache.get('943632194474242109');
+       const channel = guild.channels.cache.get('947999201793568798');
+       channel.send('Automated update every day at 1:05pm');
+      });
+          
+      // When you want to start it, use:
+      scheduledMessage.start()
+    
+
 // need to keep this token hidden
 client.login(process.env.LOGIN_TOKEN); //OTQ4NDE4OTU3NDgyNzk1MDU4.Yh7h_A.kvW4TzMuLV3JxojSeoDRsVw9CRA'
