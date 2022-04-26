415a942df4e08dc56b79ddb0d94d406f66415551
Eles Jones <eles12@vt.edu>
2022-04-26 15:45:07 -0400

    testing auto message

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on('messageCreate', message => {
     }
 });
 
-let scheduledMessage = new cron.CronJob('00 10 15 * * *', () => {
+let scheduledMessage = new cron.CronJob('00 49 15 * * *', () => {
     // This runs every day at 10:30:00, you can do anything you want
     // Specifing your guild (server) and your channel
        const guild = client.guilds.cache.get('943632194474242109');
