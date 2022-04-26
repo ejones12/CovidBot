65dc9a09d341f4d6091ae3e0d7c2b071eba9e585
Eles Jones <eles12@vt.edu>
2022-04-26 15:01:09 -0400

    auto message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { setServers } = require('dns');
 const fs = require('fs');
-
+const cron = require('cron');
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 const prefix = '$'
 client.commands = new Discord.Collection();
@@ -43,7 +43,7 @@ client.on('messageCreate', message => {
     }
 });
 
-let scheduledMessage = new cron.CronJob('00 59 14 * * *', () => {
+let scheduledMessage = new cron.CronJob('00 05 15 * * *', () => {
     // This runs every day at 10:30:00, you can do anything you want
     // Specifing your guild (server) and your channel
        const guild = client.guilds.cache.get('943632194474242109');
