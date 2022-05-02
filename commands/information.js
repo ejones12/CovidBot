4614b04381b613194ea56641facd3ece4d3d9f8d
Camila Arbaiza <camilaarb@vt.edu>
2022-05-02 18:03:07 -0400

    added reading from json file implementation

--- commands/information.js
+++ commands/information.js
@@ -1,3 +1,23 @@
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+// readTextFile("/Users/Documents/workspace/test.json", function(text){
+//     var data = JSON.parse(text);
+//     console.log(data);
+// });
+
+
 module.exports = {
     name: 'information',
     description: "Allows the user to request immediate updates.",
@@ -11,7 +31,12 @@ module.exports = {
             //read from json?
             //need check for args to see if invalid
             //pass in as args to twitter script
-
+            readTextFile("./general_data.json", function(text){
+                var data = JSON.parse(text);
+                //console.log(data);
+                message.channel.send(data[0].text);
+            });
+        
         });
     }
 }
