16cbc576d5356a68afe9c1139f5ff50a4ba5542f
ryanmaxey6 <ryanmaxey6@vt.edu>
2022-05-02 19:03:27 -0400

    Added listeners in information.js

--- commands/information.js
+++ commands/information.js
@@ -4,14 +4,22 @@ module.exports = {
     execute(message, args, subscriptions) {
         const { spawn } = require('child_process');
         const scrapingProg = spawn('python', ['./twitterScraper.py', 'general']);
-        console.log('Started child process for twitter scraping...')
-        
+        // const scrapingProg = spawn('ls', []);
+        console.log('Started child process for twitter scraping...');
+
+        scrapingProg.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
         scrapingProg.stdout.on('data', function(data) {
             console.log('anything');
             console.log(data.toString());
             //read from json?
             //need check for args to see if invalid
             //pass in as args to twitter script
+        });
+
+        scrapingProg.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
         });
     }
 }
