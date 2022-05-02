9aac2774c1d825c9f462bf98d5ea056214d36369
Camila Arbaiza <camilaarb@vt.edu>
2022-05-02 18:56:29 -0400

    completed functionality for reading from json file

--- commands/information.js
+++ commands/information.js
@@ -1,22 +1,4 @@
-import sfnadfja
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-
-//usage:
-// readTextFile("/Users/Documents/workspace/test.json", function(text){
-//     var data = JSON.parse(text);
-//     console.log(data);
-// });
-
+const fs = require('fs')
 
 module.exports = {
     name: 'information',
@@ -25,18 +7,27 @@ module.exports = {
         const { spawn } = require('child_process');
         const scrapingProg = spawn('python', ['./twitterScraper.py', 'general']);
         console.log('Started child process for twitter scraping...')
-        
+
         scrapingProg.stdout.on('data', function(data) {
             console.log(data.toString());
             //read from json?
             //need check for args to see if invalid
             //pass in as args to twitter script
-            readTextFile("./general_data.json", function(text){
-                var data = JSON.parse(text);
-                //console.log(data);
-                message.channel.send(data[0].text);
+            fs.readFile('./general_data.json', 'utf8', (err, jsonString) => {
+                if (err) {
+                    console.log("Error reading file from disk:", err)
+                    return
+                }
+                try {
+                    const data = JSON.parse(jsonString)
+                    console.log(data.data[0].text);
+                    //console.log("Customer address is:", customer.address) // => "Customer address is: Infinity Loop Drive"
+                    message.channel.send(data.data[0].text);
+            } catch(err) {
+                    console.log('Error parsing JSON string:', err)
+                }
             });
-        
+
         });
     }
 }
