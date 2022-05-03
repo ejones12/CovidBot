9ba9359eca6c16213d3fbcc615c4735a032d238f
Camila Arbaiza <camilaarb@vt.edu>
2022-05-03 07:24:33 -0400

    added pytests

---
+++ twitterScraperTest.py
@@ -0,0 +1,33 @@
+import pytest
+
+import twitterScraper
+
+"""
+    Unit tests for twitterScraper.py using pytest
+"""
+
+#command to run: python -m pytest ./twitterScraperTest.py 
+
+#Tests that the search_twitter function creates a non-empty json file with the "masks" keyword
+def test_masks():
+    twitterScraper.search_twitter("masks", twitterScraper.get_related("masks"))
+    file = open("masks_data.json",'r')
+    assert file.read() != None
+
+#Tests that the search_twitter function creates a non-empty json file with the "vaccines" keyword
+def test_vaccines():
+    twitterScraper.search_twitter("vaccines", twitterScraper.get_related("vaccines"))
+    file = open("vaccines_data.json",'r')
+    assert file.read() != None
+
+#Tests that the search_twitter function creates a non-empty json file with the "travel" keyword
+def test_travel(tmpdir):
+    twitterScraper.search_twitter("travel", twitterScraper.get_related("travel"))
+    file = open("travel_data.json",'r')
+    assert file.read() != None
+
+#Tests that the search_twitter function creates a non-empty file with the "general" keyword
+def test_general(tmpdir):
+    twitterScraper.search_twitter("general", twitterScraper.get_related("general"))
+    file = open("general_data.json",'r')
+    assert file.read() != None
